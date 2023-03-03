df1148e78ba64b8c6ff01e9786e08910751ea04e
vitordiniz25 <vitordiniz25@gmail.com>
2023-03-02 23:50:08 -0300

    criando database local

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http';
+import { Database } from './database.js';
 import { json } from './middlewares/json.js';
 
-const users = []
+const database = new Database
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req
@@ -10,23 +11,25 @@ const server = http.createServer(async (req, res) => {
 
 
     if (method === 'GET' && url === '/users'){
-        return res
-        .setHeader('Content-type', 'application/json')
-        .end(JSON.stringify(users))
+        const users = database.select('users')
+        
+        return res.end(JSON.stringify(users))
     }
 
     if (method === 'POST' && url === '/users'){
         const { name, email } = req.body
 
-        users.push({
+        const user ={
             id:1,
             name,
             email
-        })
+        }
+
+        database.insert('users', user)
 
         return res.writeHead(201).end()
     }
-
+    
     return res.writeHead(404).end('Not found')
 })
 
