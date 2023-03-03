1c5e08543e89040bae68749c56fcad0dc7db609b
vitordiniz25 <vitordiniz25@gmail.com>
2023-03-03 18:48:45 -0300

    usando regex nos route params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,6 +2,9 @@
 
 export function buildRoutePath(path) {
     const routeParametersRegex = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)')
 
-    return new RegExp()
+    const pathRegex = new RegExp(`^${pathWithParams}`)
+
+    return pathRegex
 }
